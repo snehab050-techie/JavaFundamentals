30a2baa9c742d61d5b2aae01db7496e1b9fda584
snehab050-techie <snehab050@gmail.com>
2026-07-24 20:20:01 +0530

    Committing changes worked on 7 different looping techniques to print list objects

--- src/JavaCollections/List/ShoppingCart.java
+++ src/JavaCollections/List/ShoppingCart.java
@@ -1,14 +1,52 @@
 package JavaCollections.List;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
 
 //ArrayList
 public class ShoppingCart {
 
     public static void viewMyCart(ArrayList<String> myCart){
+
+        // for each loop
+        System.out.println("------------------");
         for(String myCartItem: myCart){
             System.out.println(myCartItem);
         }
+
+        //traditional for loop
+        System.out.println("------------------");
+        for(int i= 0;i<myCart.size();i++){
+            System.out.println(myCart.get(i));
+        }
+
+        //for each method
+        System.out.println("------------------");
+        myCart.forEach(item -> System.out.println(item));
+
+        //method referencing
+        System.out.println("------------------");
+        myCart.forEach(System.out::println);
+
+        //iteartor()
+        System.out.println("------------------");
+        Iterator<String> it = myCart.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+        //ListIterator
+        System.out.println("------------------");
+        ListIterator<String> lit = myCart.listIterator();
+        while(lit.hasNext()){
+            System.out.println(lit.next());
+        }
+
+        //streams - Java v8
+        System.out.println("------------------");
+        myCart.stream().filter(item -> item.startsWith("S")).forEach(System.out::println);
+
     }
 
     static void main() {
@@ -18,8 +56,10 @@ public class ShoppingCart {
         shoppingCart.add("Brush");
         shoppingCart.add("Book");
         shoppingCart.add("Cloth");
-        shoppingCart.add("Pen");
+        shoppingCart.add("Shells");
 
         ShoppingCart.viewMyCart(shoppingCart);
+
+
     }
 }
