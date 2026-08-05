c4605a691a83eb0e08b3e7a194be5b4aedd01076
snehab050-techie <snehab050@gmail.com>
2026-08-05 23:34:01 +0530

    Program to find duplicate element in an array

--- src/Java_Coding_Questions/Ques1.java
+++ src/Java_Coding_Questions/Ques1.java
@@ -1,8 +1,9 @@
 package Java_Coding_Questions;
 
-//find first non-repeating character in a String
 public class Ques1 {
     static void main() {
+
+        //find first non-repeating character in a String
         String name = "wiiwssdom";
         //output = i
         char[] arr = name.toCharArray();
@@ -17,6 +18,22 @@ public class Ques1 {
             if(count == 1){
                 System.out.println("The first non repeating char is: "+arr[i]);
                 break;
+            }
+        }
+
+        // Find duplicate element in array
+        int[] numArr = {10,78,91,10,91};
+
+        for(int i=0;i<numArr.length;i++){
+            int count = 0;
+            for(int j=0;j<numArr.length;j++) {
+                if (numArr[i] == numArr[j]) {
+                    count++;
+                }
+                if (count > 1) {
+                    System.out.println("Duplicate element is: " + numArr[i]);
+                    break;
+                }
             }
         }
     }
